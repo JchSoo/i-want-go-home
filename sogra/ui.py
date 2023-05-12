13be472df98c9b0a3f6887b593c7b0969a61fb02
rlqja <rlqja1220@gmail.com>
2023-05-13 07:31:15 +0900

    1

--- sogra/ui.py
+++ sogra/ui.py
@@ -3,26 +3,24 @@ from PIL import ImageTk
 from tkinter import ttk
 import threading
 import tkinter.font
-from PIL import ImageTk, Image
 
 window = tkinter.Tk()
 window.title("I want to go home")
-window.geometry("800x450+375+150")
+window.geometry("800x480+375+150")
 window.resizable(False, False)
-count = 0
-state = ""
-st = ""
+count = 0 #클릭 횟수
+nowstate = 0 #현재이미지번호?
+gifplaying = False #스즈메 진행중?
 font1=tkinter.font.Font(family="맑은 고딕", size=20, weight='bold')
 top = tkinter.Label(window, text = "집에 가고 싶을 때 마다 누르는 버튼", font = font1)
 top.pack()
 
 def gui():
-    font2=tkinter.font.Font(family="맑은 고딕", size=20, weight="bold")
-
     global button
+
     def click():
         global count
-        global st
+        global gifplaying
         global real
         if count < 100:
             count += 1
@@ -34,8 +32,9 @@ def gui():
             count = 100
             prog_bar["value"] = count
             prog_bar.update()
-            st = "1"
+            gifplaying = True
             button.configure(state = 'disabled')
+            font2=tkinter.font.Font(family="맑은 고딕", size=20, weight="bold")
             real = tkinter.Label(window, text = "되겠냐?", font = font2, state="active", activeforeground="red")
             real.pack()
             update(0)
@@ -45,19 +44,19 @@ def gui():
 
     prog_bar = ttk.Progressbar(window, length=300, maximum=100)
     prog_bar.pack()
-    # prog_bar.grid(row=1, column=0, padx=10, pady=10)
 
     button = tkinter.Button(window, text = "아~ 집가고 싶다~!", command = click, width=20, height=2)
     button.pack()
 
     myimage = ImageTk.PhotoImage(file = "./image/door0.png")
+
     bottom = tkinter.Label(window, image = myimage)
     bottom.pack()
 
     def update(ind):
         global myimage
         global count
-        global st
+        global gifplaying
         global real
         
         ind += 1
@@ -65,54 +64,56 @@ def gui():
         if ind < 10 and ind >= 0:
             myimage = ImageTk.PhotoImage(file = f"./suzume/suzume-000{ind}.jpg")
             bottom.config(image = myimage)
+
         elif ind < 96:
             myimage = ImageTk.PhotoImage(file = f"./suzume/suzume-00{ind}.jpg")
             bottom.config(image = myimage)
+
         else:
             myimage = ImageTk.PhotoImage(file = f"./image/door0.png")
             bottom.config(image = myimage)
             count = 0
             prog_bar["value"] = count
             prog_bar.update()
-            st = ""
+            gifplaying = False
             button.configure(state = 'normal')
             real.pack_forget()
+
             return
         
         window.after(40, update, ind)
 
     def ChangeImage(num):
         global myimage
-        global state
-
-        myimage = num
-        state = num
+        global nowstate
 
         myimage = ImageTk.PhotoImage(file = f"./image/door{num}.png")
         bottom.config(image = myimage)
 
     def dec():
-        global st
+        global gifplaying
         global count
-        if count > 0 and st == "":
+
+        if count > 0 and gifplaying == False: #횟수 0이상이고 스즈메 플레이 안하고 있으면
             count -= 1
             check()
             prog_bar["value"] = count
             prog_bar.update()
-        a = 1 - (count / 110)
-        threading.Timer(a, dec).start()
+
+        tick = 1 - (count / 110)
+        threading.Timer(tick, dec).start()
 
     def check():
         global count
-        global state
+        global nowstate
+
         thiscase = (count // 2) * 2
 
-        if state != thiscase:
+        if nowstate != thiscase: #이미지 바꿀 타이밍이면 바꿈
             ChangeImage(thiscase)
 
     dec()
 
     window.mainloop()
 
-if __name__ == '__main__':
-    gui()
+gui()
