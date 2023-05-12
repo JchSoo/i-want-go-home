ca5316ee640e5aa105ef9c028c712615e4ff7227
wpemdoghrk <127833977+wpemdoghrk@users.noreply.github.com>
2023-05-12 23:46:58 +0900

    hello

--- sogra/joke.py
+++ sogra/joke.py
@@ -1,19 +1,15 @@
 import tkinter
 from PIL import ImageTk
 
-
 window = tkinter.Tk()
 window.title("I want to go home")
 window.geometry("800x450+375+150")
 window.resizable(False, False)
 
-
-top = tkinter.Label(window, text = "집에 가고 싶을 때 마다 누르는 버튼")
+top = tkinter.Label(window, text = "집 가고 싶을 때 누르는 버튼")
 top.pack()
 
-
 count = 0
-
 
 def click():
     global count
@@ -28,19 +24,15 @@ def click():
     if count == 20:
         change_b()
     
-
 middle = tkinter.Label(window, text = f"{count}번 생각함")
 middle.pack()
-
 
 button = tkinter.Button(window, text = "아~ 집가고 싶다~!", command = click)
 button.pack()
 
-
 myimage = ImageTk.PhotoImage(file = "chaewon.png")
 bottom = tkinter.Label(window, image = myimage)
 bottom.pack()
-
 
 def change_a():
     global myimage2
@@ -48,12 +40,10 @@ def change_a():
     myimage2 = ImageTk.PhotoImage(file = "chaewon_two.png")
     bottom.config(image = myimage2)
 
-
 def change_b():
     global myimage3
 
     myimage3 = ImageTk.PhotoImage(file = "kazuha.png")
     bottom.config(image = myimage3)
-
 
 window.mainloop()
---
+++ sogra/var.py
@@ -0,0 +1,14 @@
+# def select(self):
+#     value = "값 : " + str(scale.get())
+#     label.config(text = value)
+
+# var = tkinter.IntVar()
+
+# scale = tkinter.Scale(window, variable = var, command = select, 
+#                       orient = "horizontal", width = 10, bg = 'pink', 
+#                       troughcolor = 'pink', sliderlength = 25, 
+#                       tickinterval = 20, length = 300)
+# scale.pack()
+
+# label = tkinter.Label(window, text = "값 : 0")
+# label.pack()
