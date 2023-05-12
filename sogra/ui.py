85f62e7523dc9425afda55a5a5bd71cf76cdaf9d
rlqja <rlqja1220@gmail.com>
2023-05-12 23:56:25 +0900

    hahahah

--- sogra/ui.py
+++ sogra/ui.py
@@ -1,59 +1,66 @@
 import tkinter
 from PIL import ImageTk
-
+import threading
 
 window = tkinter.Tk()
 window.title("I want to go home")
 window.geometry("800x450+375+150")
 window.resizable(False, False)
-
+count = 0
 
 top = tkinter.Label(window, text = "집에 가고 싶을 때 마다 누르는 버튼")
 top.pack()
 
+def gui():
+    def click():
+        global count
+        
+        count += 1
 
-count = 0
+        middle.config(text = f"{count}번 생각함")
 
+        if count == 10:
+            change_a()
 
-def click():
-    global count
-    
-    count += 1
+        if count == 20:
+            change_b()
+        
 
-    middle.config(text = f"{count}번 생각함")
-
-    if count == 10:
-        change_a()
-
-    if count == 20:
-        change_b()
-    
-
-middle = tkinter.Label(window, text = f"{count}번 생각함")
-middle.pack()
+    middle = tkinter.Label(window, text = f"{count}번 생각함")
+    middle.pack()
 
 
-button = tkinter.Button(window, text = "아~ 집가고 싶다~!", command = click)
-button.pack()
+    button = tkinter.Button(window, text = "아~ 집가고 싶다~!", command = click)
+    button.pack()
 
 
-myimage = ImageTk.PhotoImage(file = "chaewon.png")
-bottom = tkinter.Label(window, image = myimage)
-bottom.pack()
+    myimage = ImageTk.PhotoImage(file = "chaewon.png")
+    bottom = tkinter.Label(window, image = myimage)
+    bottom.pack()
 
 
-def change_a():
-    global myimage2
+    def change_a():
+        global myimage2
 
-    myimage2 = ImageTk.PhotoImage(file = "chaewon_two.png")
-    bottom.config(image = myimage2)
-
-
-def change_b():
-    global myimage3
-
-    myimage3 = ImageTk.PhotoImage(file = "kazuha.png")
-    bottom.config(image = myimage3)
+        myimage2 = ImageTk.PhotoImage(file = "chaewon_two.png")
+        bottom.config(image = myimage2)
 
 
-window.mainloop()
+    def change_b():
+        global myimage3
+
+        myimage3 = ImageTk.PhotoImage(file = "kazuha.png")
+        bottom.config(image = myimage3)
+
+    def dec():
+        global count
+        if count != 0:
+            count -= 1
+            middle.config(text = f"{count}번 생각함")
+        threading.Timer(0.5, dec).start()
+
+    dec()
+
+    window.mainloop()
+
+gui()
